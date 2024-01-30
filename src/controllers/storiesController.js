136c75ada530cd9733b23631c8488a30e5c2a3d7
KimYoungJin <kyj732422@gmail.com>
2024-01-31 02:39:51 +0900

    CRUDS

--- src/controllers/storiesController.js
+++ src/controllers/storiesController.js
@@ -3,10 +3,10 @@ import Video from "../models/Video";
 export const home = async (req, res) => {
   const videos = await Video.find({});
   console.log(videos);
-  return res.render("home", { path: "home", videos });
+  return res.render("home", { path: "Home", videos });
 };
 export const upload = (req, res) => {
-  res.render("upload", { path: "upload" });
+  res.render("upload", { path: "Upload" });
 };
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
@@ -23,13 +23,9 @@ export const postUpload = async (req, res) => {
     return res.redirect("/");
   } catch (error) {
     console.log("I find error ❌❌", error);
-    res.render("upload", { path: "upload", error: error._message });
+    res.render("upload", { path: "Upload", error: error._message });
   }
 };
-export const trending = (req, res) =>
-  res.render("stories", { path: "trending" });
-export const newStories = (req, res) =>
-  res.render("stories", { path: "newStories" });
 export const seeStory = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
@@ -37,7 +33,7 @@ export const seeStory = async (req, res) => {
     return res.render("404", { path: "404" });
   }
   return res.render("stories", {
-    path: "seeStory",
+    path: "SeeStory",
     video,
     id: `${req.params.id}`,
   });
@@ -72,4 +68,18 @@ export const deleteStory = async (req, res) => {
   const { id } = req.params;
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  console.log(keyword);
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}`, "i"),
+      },
+    });
+  }
+  return res.render("search", { path: "Search", videos });
 };
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { home, upload, postUpload } from "../controllers/storiesController";
+import {
+  home,
+  upload,
+  postUpload,
+  search,
+} from "../controllers/storiesController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
 globalRouter.route("/upload").get(upload).post(postUpload);
+globalRouter.route("/search").get(search);
 
 export default globalRouter;
